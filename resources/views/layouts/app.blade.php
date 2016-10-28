6254ee6cf515120ba88966d019854062d1694b19
gozaing <steganographi@gmail.com>
2016-10-28 20:26:06 +0900

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Actmon
                 </a>
             </div>
 
