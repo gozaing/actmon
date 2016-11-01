0d119ef9a6cded15ba6987206945d49df9dffe0d
gozaing <steganographi@gmail.com>
2016-11-01 16:15:02 +0900

    add help tool

--- config/app.php
+++ config/app.php
@@ -156,6 +156,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * help tool
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +207,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /**
+         * help tool
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
